a17d4131460ed8bbe1e6029003258f0704984691
kirito <504831261@qq.com>
2023-10-13 21:42:34 +0800

    vue3 study

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <the-resource></the-resource>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheResource from "./components/learning-resources/TheResource.vue";
+
+export default {
+    components: {
+      TheResource,
+      TheHeader
+    },
+    data() {
+        return {};
+    }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+
+html{
+  font-family: 'Roboto',sans-serif;
+}
+
+body{
+  margin: 0;
+}
+</style>
