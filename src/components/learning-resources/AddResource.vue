e49a385b8c6de9eca0b91f358e488c07fda393d9
kirito <504831261@qq.com>
2023-10-15 20:12:02 +0800

    BaseDialog

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately,at least one input value is invalid.</p>
+      <p>Please check all inputs and make sure you enter at least a few characters into each input field.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -21,16 +30,32 @@
 </template>
 
 <script>
+import BaseDialog from "../UI/BaseDialog.vue";
+import BaseButton from "../UI/BaseButton.vue";
+
 export default {
-  inject:['addResource'],
+  components: {BaseButton, BaseDialog},
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDesc = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
 
+      if (enteredTitle === '' || enteredDesc === '' || enteredUrl === '') {
+        this.inputIsInvalid = true;
+        return;
+      }
       this.addResource(enteredTitle, enteredDesc, enteredUrl);
     },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
   }
 }
 </script>
